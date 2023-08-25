1b7a8cf5707d6e34dea478cd8c9957e506b0959b
KenChiimken <134357060+KenChiimken@users.noreply.github.com>
2023-08-25 16:01:36 +0800

    Week 1
    must continue next meeting

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using JetBrains.Annotations;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public GameObject Test_UI
+        public Vector3 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
